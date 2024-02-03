3ee9c90e01e6835ed2000c1a5d6a37ddc08fbcd6
Ilia <ilia.iliev.2013@uni.strath.ac.uk>
2024-02-03 18:46:36 +0200

    readme now includes curl command for testing. Updated fastapi endpoint

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 Proof of concept service which, given an image, generates an image description
+
+
+```
+curl -X 'POST' \
+'http://localhost:8000/image2caption/blip' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: multipart/form-data' \
+  -F 'image=@example.jpg;type=image/jpeg'
+  ```
--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile
+from PIL import Image
+from caption_extractor import CachedPipeline, model_shortener
+
 
 app = FastAPI()
-
+model = None
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+@app.post("/uploadimage/")
+def create_upload_image(image: UploadFile):
+    #TODO: check that it's an image, check size is up to 1024x1024
+    return {"filename": image.filename}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.post("/image2caption/{model}")
+async def image2caption(image: UploadFile, model: str):
+    full_name = model_shortener.get(model)
+    pipeline = CachedPipeline.get(full_name)
+    converted_image = Image.open(image.file)
+    return {"caption": pipeline(converted_image)}
